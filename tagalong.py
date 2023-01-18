38b98514b0597ce507f1a3e075804f0058be666f
Julian Vetter <jvetter@kalrayinc.com>
2023-01-18 09:28:30 +0100

    Wrap everthing in a try/except KeyboardInterrupt
    If the user presses Ctrl+c while tagalong searches the database for
    matching tags there won't be a backtrace.

--- tagalong.py
+++ tagalong.py
@@ -7,63 +7,67 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-import argparse
-from pathlib import Path
-import os
-from tinydb import TinyDB, Query
+try:
+    import argparse
+    from pathlib import Path
+    import os
+    from tinydb import TinyDB, Query
 
-def main():
-    """
-    main()
-    """
-    query = Query()
+    def main():
+        """
+        main()
+        """
+        query = Query()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a",
-                        dest="add_tag",
-                        help="Add a tag to a file or folder",
-                        type=str,
-                        required=False)
-    parser.add_argument("-d",
-                        dest="del_tag",
-                        help="Delete a tag from a file or folder",
-                        type=str,
-                        required=False)
-    parser.add_argument("filename",
-                        help="Operate on the given file or folder")
-    args = parser.parse_args()
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-a",
+                            dest="add_tag",
+                            help="Add a tag to a file or folder",
+                            type=str,
+                            required=False)
+        parser.add_argument("-d",
+                            dest="del_tag",
+                            help="Delete a tag from a file or folder",
+                            type=str,
+                            required=False)
+        parser.add_argument("filename",
+                            help="Operate on the given file or folder")
+        args = parser.parse_args()
 
-    tagalong_db_path = os.path.expanduser("~/.config/tagalong")
-    if not os.path.exists(tagalong_db_path):
-        os.mkdir(tagalong_db_path)
-    tagalong_db = TinyDB("{:s}/tagalong_db.json".format(tagalong_db_path))
+        tagalong_db_path = os.path.expanduser("~/.config/tagalong")
+        if not os.path.exists(tagalong_db_path):
+            os.mkdir(tagalong_db_path)
+        tagalong_db = TinyDB("{:s}/tagalong_db.json".format(tagalong_db_path))
 
-    folder = tagalong_db.get(query.folder == args.filename)
+        folder = tagalong_db.get(query.folder == args.filename)
 
-    if args.add_tag:
-        # Get all unique elements from the taglist
-        tag_list = list(set(args.add_tag.split(",")))
-        if folder:
-            tag_list = list(set(tag_list + folder["tags"]))
-            tagalong_db.update({"tags" : tag_list}, query.folder == args.filename)
+        if args.add_tag:
+            # Get all unique elements from the taglist
+            tag_list = list(set(args.add_tag.split(",")))
+            if folder:
+                tag_list = list(set(tag_list + folder["tags"]))
+                tagalong_db.update({"tags" : tag_list}, query.folder == args.filename)
+            else:
+                tagalong_db.insert({"folder" : args.filename, "tags" : tag_list})
+        elif args.del_tag:
+            if folder:
+                tag_list  = list(set(folder["tags"]) - set(args.del_tag.split(",")))
+                if not len(tag_list):
+                    tagalong_db.remove(query.folder == args.filename)
+                tagalong_db.update({"tags" : tag_list}, query.folder == args.filename)
         else:
-            tagalong_db.insert({"folder" : args.filename, "tags" : tag_list})
-    elif args.del_tag:
-        if folder:
-            tag_list  = list(set(folder["tags"]) - set(args.del_tag.split(",")))
-            if not len(tag_list):
-                tagalong_db.remove(query.folder == args.filename)
-            tagalong_db.update({"tags" : tag_list}, query.folder == args.filename)
-    else:
-        while(args.filename is not "/"):
-            folder = tagalong_db.get(query.folder == args.filename)
-            if folder and len(folder["tags"]):
-                print(*folder["tags"], sep=", ")
-                break
-            path = Path(args.filename)
-            args.filename = str(path.parent.absolute())
-        if not folder:
-            print("_")
+            while(args.filename is not "/"):
+                folder = tagalong_db.get(query.folder == args.filename)
+                if folder and len(folder["tags"]):
+                    print(*folder["tags"], sep=", ")
+                    break
+                path = Path(args.filename)
+                args.filename = str(path.parent.absolute())
+            if not folder:
+                print("_")
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
+
+except KeyboardInterrupt:
+    pass
